2564cb3ebdfc0c3c77c127ac593fef990c0aa64c
romain.ferrari@thalesgroup.com <romain.ferrari@thalesgroup.com@b5d8583f-9428-48d8-90d7-9fcbe50e6af6>
2012-07-26 12:59:58 +0000

    Bug fix because of the previous refactoring. com.authzforce.impl.AuthZforce was called instead of com.thalesgroup.authzforce.soap.api.AuthZForce, xsd and wsdl added
    git-svn-id: http://svn.theresis.org/svn/gt-appsec/AuthZForce/AuthZForce/trunk@2155 b5d8583f-9428-48d8-90d7-9fcbe50e6af6

--- interface/soap/src/main/java/com/thalesgroup/authzforce/soap/impl/AuthzForceImpl.java
+++ interface/soap/src/main/java/com/thalesgroup/authzforce/soap/impl/AuthzForceImpl.java
@@ -8,7 +8,7 @@ import oasis.names.tc.xacml._2_0.context.schema.os.ResponseType;
 import com.thalesgroup.authzforce.soap.api.AuthzForce;
 
 @SuppressWarnings("restriction")
-@WebService(serviceName = "AuthzForce", portName = "AuthzForceSOAP", endpointInterface = "com.thalesgroup.pdp.AuthzForce", targetNamespace = "http://pdp.thalesgroup.com/")
+@WebService(serviceName = "AuthzForce", portName = "AuthzForceSOAP", endpointInterface = "com.thalesgroup.authzforce.soap.api.AuthzForce", targetNamespace = "http://pdp.thalesgroup.com/")
 public class AuthzForceImpl implements AuthzForce {
 
 	public AuthzForceImpl() {
