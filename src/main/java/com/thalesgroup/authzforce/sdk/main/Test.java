1e2f949dc70474ecbe234c371c9fc6a3337ad68c
romain.ferrari@thalesgroup.com <romain.ferrari@thalesgroup.com@b5d8583f-9428-48d8-90d7-9fcbe50e6af6>
2013-01-16 17:09:28 +0000

    Cleaning code and getting rude of map and list based methods
    git-svn-id: http://svn.theresis.org/svn/gt-appsec/AuthZForce/AuthZForce/trunk@2703 b5d8583f-9428-48d8-90d7-9fcbe50e6af6

--- src/main/java/com/thalesgroup/authzforce/sdk/main/Test.java
+++ src/main/java/com/thalesgroup/authzforce/sdk/main/Test.java
@@ -2,10 +2,7 @@ package com.thalesgroup.authzforce.sdk.main;
 
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.thalesgroup.authzforce.sdk.XacmlSdk;
 import com.thalesgroup.authzforce.sdk.core.schema.Action;
@@ -14,7 +11,6 @@ import com.thalesgroup.authzforce.sdk.core.schema.Resource;
 import com.thalesgroup.authzforce.sdk.core.schema.Response;
 import com.thalesgroup.authzforce.sdk.core.schema.Responses;
 import com.thalesgroup.authzforce.sdk.core.schema.Subject;
-import com.thalesgroup.authzforce.sdk.core.schema.XACMLAttributeId;
 import com.thalesgroup.authzforce.sdk.core.schema.XACMLDatatypes;
 import com.thalesgroup.authzforce.sdk.exceptions.XacmlSdkException;
 import com.thalesgroup.authzforce.sdk.xacml.utils.XacmlSdkImpl;
@@ -67,51 +63,4 @@ public class Test {
 					+ responses.getSubject());
 		}
 	}
-	
-	private static void mainListMap() {
-		List<Map<XACMLAttributeId, String>> resourceList;
-
-		Map<XACMLAttributeId, String> mySubject = null;
-		Map<XACMLAttributeId, String> myResource = null;
-		Map<XACMLAttributeId, String> myResource2 = null;
-		Map<XACMLAttributeId, String> myAction = null;
-		Map<XACMLAttributeId, String> myEnvironment = null;
-
-		resourceList = new ArrayList<Map<XACMLAttributeId, String>>();
-
-		mySubject = new HashMap<XACMLAttributeId, String>();
-		myResource = new HashMap<XACMLAttributeId, String>();
-		myResource2 = new HashMap<XACMLAttributeId, String>();
-		myAction = new HashMap<XACMLAttributeId, String>();
-		myEnvironment = new HashMap<XACMLAttributeId, String>();
-
-		mySubject.put(XACMLAttributeId.XACML_SUBJECT_SUBJECT_ID, SUBJECT);
-		myResource.put(XACMLAttributeId.XACML_RESOURCE_RESOURCE_ID, RESOURCE);
-		myResource2
-				.put(XACMLAttributeId.XACML_RESOURCE_RESOURCE_ID, RESOURCE_2);
-		myAction.put(XACMLAttributeId.XACML_ACTION_ACTION_ID, ACTION);
-		myEnvironment.put(XACMLAttributeId.XACML_SUBJECT_AUTHENTICATION_TIME,
-				new Date().toString());
-
-		resourceList.add(myResource);
-		resourceList.add(myResource2);
-
-		XacmlSdk myXacml = new XacmlSdkImpl(URI.create(PDP_ENDPOINT));
-
-		Responses responses = null;
-		try {
-			responses = myXacml.getAuthZ(mySubject, resourceList, myAction,
-					myEnvironment);
-		} catch (XacmlSdkException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		for (Response response : responses.getResponse()) {
-			System.out.println(response.getAction() + " on "
-					+ response.getResourceId() + ": " 
-					+ response.getDecision().value() + " for " 
-					+ responses.getSubject());
-		}
-	}
-
 }
