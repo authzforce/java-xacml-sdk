4deeb527bb6381f65238e5415c37fd14f92ab955
hargathor <3949704+hargathor@users.noreply.github.com>
2013-07-05 14:40:45 +0200

    implementing equals method on Resource schema and changing pom version to 3.1.5

--- src/main/java/com/thalesgroup/authzforce/sdk/core/schema/Resource.java
+++ src/main/java/com/thalesgroup/authzforce/sdk/core/schema/Resource.java
@@ -3,6 +3,9 @@ package com.thalesgroup.authzforce.sdk.core.schema;
 import java.io.Serializable;
 import java.util.List;
 
+import org.jvnet.jaxb2_commons.lang.EqualsStrategy;
+import org.jvnet.jaxb2_commons.locator.ObjectLocator;
+
 import oasis.names.tc.xacml._3_0.core.schema.wd_17.AttributeType;
 import oasis.names.tc.xacml._3_0.core.schema.wd_17.AttributeValueType;
 
@@ -27,5 +30,11 @@ public class Resource extends AttributeType {
 
 	public List getAttributes() {
 		return this.attributeValue;
+	}
+	
+	@Override
+	public boolean equals(ObjectLocator thisLocator, ObjectLocator thatLocator,
+			Object object, EqualsStrategy strategy) {
+		return super.equals(thisLocator, thatLocator, object, strategy);
 	}
 }
