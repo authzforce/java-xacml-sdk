d75f52dafc874f1b4de0d0a83497f8a9e44acbff
hargathor <3949704+hargathor@users.noreply.github.com>
2015-10-23 17:01:22 +0200

    Ajout de dependances de tests. Retour en arrière sur les mocks

--- authzforce-xacmlsdk-core/src/test/java/com/thalesgroup/authzforce/sdk/xacml/tests/TestPdpNetworkErrors.java
+++ authzforce-xacmlsdk-core/src/test/java/com/thalesgroup/authzforce/sdk/xacml/tests/TestPdpNetworkErrors.java
@@ -4,21 +4,13 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.cxf.endpoint.Server;
-import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
-import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.thalesgroup.authzforce.api.jaxrs.EndUserDomainSet;
 import com.thalesgroup.authzforce.sdk.core.schema.Action;
 import com.thalesgroup.authzforce.sdk.core.schema.Environment;
 import com.thalesgroup.authzforce.sdk.core.schema.Resource;
@@ -36,27 +28,9 @@ public class TestPdpNetworkErrors {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(TestPdpNetworkErrors.class);
 	private final static String ENDPOINT_ADDRESS = "http://127.0.0.1:7777/";
-	private static Server server;
 	public final XacmlSdkImpl sdk = new XacmlSdkImpl(URI.create(ENDPOINT_ADDRESS), USER_DOMAIN);
 	
-	@Mock EndUserDomainSet domainSet;
-	@Mock JAXRSClientFactory mockedJaxrs;
-	@InjectMocks public XacmlSdkImpl mockedSdk;
-	
-	@Before
-	public void setUp() {
-		LOGGER.info("SETUP CONTEXT: " + this.getClass());
 
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"classpath:META-INF/spring/beans.xml"});
-		JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
-		List<Object> serviceBeans = new ArrayList<Object>();
-		serviceBeans.add(ctx.getBean("domainsResourceBean"));
-		sf.setServiceBeans(serviceBeans);
-		sf.setAddress(ENDPOINT_ADDRESS);
-		sf.setResourceClasses(EndUserDomainSet.class);
-		
-		server = sf.create();
-	}
 
 	/**
 	 * This test has be written to check that the SDK is actually throwing
@@ -83,7 +57,7 @@ public class TestPdpNetworkErrors {
 		actionList.add(new Action("actionId"));
 		actionList.add(new Action("secondAction"));		
 		
-		Responses response = mockedSdk.getAuthZ(new Subject("subjectId") , new Resource("resourceId"),
+		Responses response = sdk.getAuthZ(new Subject("subjectId") , new Resource("resourceId"),
 				actionList, new Environment("environmentId"));
 	}
 	
@@ -97,7 +71,7 @@ public class TestPdpNetworkErrors {
 		resourcesList.add(new Resource("actionId"));
 		resourcesList.add(new Resource("secondAction"));
 		
-		Responses response = mockedSdk.getAuthZ(new Subject("subjectId") , resourcesList,
+		Responses response = sdk.getAuthZ(new Subject("subjectId") , resourcesList,
 				new Action("actionId"), new Environment("environmentId"));
 	}
 }
