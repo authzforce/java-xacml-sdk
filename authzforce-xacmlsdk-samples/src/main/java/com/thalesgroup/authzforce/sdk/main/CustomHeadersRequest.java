98f0d9442559e26ed4a82506b8cea4d016e04adb
hargathor <3949704+hargathor@users.noreply.github.com>
2015-10-29 14:44:17 +0100

    ajout d'un sample

---
+++ authzforce-xacmlsdk-samples/src/main/java/com/thalesgroup/authzforce/sdk/main/CustomHeadersRequest.java
@@ -0,0 +1,64 @@
+package com.thalesgroup.authzforce.sdk.main;
+
+import java.net.URI;
+
+import org.apache.cxf.jaxrs.impl.MetadataMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.thalesgroup.authzforce.sdk.core.schema.Action;
+import com.thalesgroup.authzforce.sdk.core.schema.Environment;
+import com.thalesgroup.authzforce.sdk.core.schema.Resource;
+import com.thalesgroup.authzforce.sdk.core.schema.Response;
+import com.thalesgroup.authzforce.sdk.core.schema.Responses;
+import com.thalesgroup.authzforce.sdk.core.schema.Subject;
+import com.thalesgroup.authzforce.sdk.core.schema.category.ActionCategory;
+import com.thalesgroup.authzforce.sdk.core.schema.category.EnvironmentCategory;
+import com.thalesgroup.authzforce.sdk.core.schema.category.ResourceCategory;
+import com.thalesgroup.authzforce.sdk.core.schema.category.SubjectCategory;
+import com.thalesgroup.authzforce.sdk.exceptions.XacmlSdkException;
+import com.thalesgroup.authzforce.sdk.impl.XacmlSdkImpl;
+
+public class CustomHeadersRequest {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CustomHeadersRequest.class);
+
+	private static final String PDP_ENDPOINT = "http://127.0.0.1:8080/authzforce-rest-service-4.1.3-OCW";
+	private static final String DOMAIN_ID = "5e022256-6d0f-4eb8-aa9d-77db3d4ad141";
+
+	private static final String SUBJECT = "ThalesId";
+	private static final String RESOURCE = "http://www.opencloudware.org";
+	private static final String ACTION = "HEAD";
+
+	private static final String TEST_HEADER_KEY = "SDK-SAMPLE";
+
+	public static void main(String[] args) {
+		SubjectCategory subjectCat = new SubjectCategory();
+		ResourceCategory resourceCat = new ResourceCategory();
+		ActionCategory actionCategory = new ActionCategory();
+		EnvironmentCategory environmentCategory = new EnvironmentCategory();
+
+		subjectCat.addAttribute(new Subject(SUBJECT));
+		resourceCat.addAttribute(new Resource(RESOURCE));
+		actionCategory.addAttribute(new Action(ACTION));
+		environmentCategory.addAttribute(new Environment("TEST_CustomHeaders"));
+		
+		MetadataMap<String, String> headers = new MetadataMap<String, String>();
+		headers.add(TEST_HEADER_KEY, "TEST_CustomHeaders");
+
+		XacmlSdkImpl myXacml = new XacmlSdkImpl(URI.create(PDP_ENDPOINT), DOMAIN_ID, headers);
+		Responses responses = null;
+		try {
+			responses = myXacml.getAuthZ(subjectCat, resourceCat, actionCategory, environmentCategory);
+		} catch (XacmlSdkException e) {
+			LOGGER.error(e.getLocalizedMessage());
+			LOGGER.error(e.getCause().getLocalizedMessage());
+		}
+		if (responses != null) {
+			for (Response response : responses.getResponse()) {
+				LOGGER.info(response.getAction() + " on " + response.getResourceId() + ": "
+						+ response.getDecision().value() + " for " + response.getSubject());
+			}
+		}
+	}
+}
