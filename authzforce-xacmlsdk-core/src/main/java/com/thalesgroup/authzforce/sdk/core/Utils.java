e303205b28c2a82e2421119e4ab73d7785950581
hargathor <3949704+hargathor@users.noreply.github.com>
2015-12-01 16:37:24 +0100

    Method creation to build better understanding response. New errors handled while creating request

--- authzforce-xacmlsdk-core/src/main/java/com/thalesgroup/authzforce/sdk/core/Utils.java
+++ authzforce-xacmlsdk-core/src/main/java/com/thalesgroup/authzforce/sdk/core/Utils.java
@@ -6,28 +6,26 @@ import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.thalesgroup.authzforce.sdk.core.schema.Action;
-import com.thalesgroup.authzforce.sdk.core.schema.Environment;
 import com.thalesgroup.authzforce.sdk.core.schema.Request;
-import com.thalesgroup.authzforce.sdk.core.schema.Resource;
 import com.thalesgroup.authzforce.sdk.core.schema.Responses;
-import com.thalesgroup.authzforce.sdk.core.schema.Subject;
-import com.thalesgroup.authzforce.sdk.core.schema.XACMLAttributeId;
 import com.thalesgroup.authzforce.sdk.core.schema.category.ActionCategory;
 import com.thalesgroup.authzforce.sdk.core.schema.category.Category;
 import com.thalesgroup.authzforce.sdk.core.schema.category.EnvironmentCategory;
 import com.thalesgroup.authzforce.sdk.core.schema.category.ResourceCategory;
 import com.thalesgroup.authzforce.sdk.core.schema.category.SubjectCategory;
+import com.thalesgroup.authzforce.sdk.core.utils.ResponsesFactory;
 import com.thalesgroup.authzforce.sdk.exceptions.XacmlSdkException;
 import com.thalesgroup.authzforce.sdk.exceptions.XacmlSdkExceptionCodes;
+import com.thalesgroup.authzforce.xacml._3_0.identifiers.XACMLAttributeId;
 
 import oasis.names.tc.xacml._3_0.core.schema.wd_17.Attribute;
-import oasis.names.tc.xacml._3_0.core.schema.wd_17.AttributeValueType;
 import oasis.names.tc.xacml._3_0.core.schema.wd_17.Attributes;
+import oasis.names.tc.xacml._3_0.core.schema.wd_17.DecisionType;
 import oasis.names.tc.xacml._3_0.core.schema.wd_17.Response;
 import oasis.names.tc.xacml._3_0.core.schema.wd_17.Result;
 
@@ -49,10 +47,18 @@ public final class Utils {
 	 * @return
 	 * 
 	 * @return
+	 * @throws XacmlSdkException
 	 */
 	public static Request createXacmlRequest(List<SubjectCategory> subjects, List<ResourceCategory> resources,
-			List<ActionCategory> actions, List<EnvironmentCategory> environments) {
+			List<ActionCategory> actions, List<EnvironmentCategory> environments) throws XacmlSdkException {
 		Request xacmlRequest = new Request();
+
+		if (null == subjects || null == resources || null == actions || null == environments) {
+			throw new XacmlSdkException(XacmlSdkExceptionCodes.CATEGORY_IS_NULL);
+		}
+		if (subjects.size() > 1 || resources.size() > 1 || actions.size() > 1 || environments.size() > 1) {
+			LOGGER.warn("Multiple categories: Be sure that your PDP support multiple decision profile");
+		}
 
 		LOGGER.debug("Assembling Request...");
 		try {
@@ -70,7 +76,7 @@ public final class Utils {
 				Utils.check(environment);
 			}
 		} catch (XacmlSdkException e) {
-
+			throw new XacmlSdkException(e);
 		}
 
 		environmentCategory.addAll(environments);
@@ -88,8 +94,10 @@ public final class Utils {
 		if (LOGGER.isDebugEnabled()) {
 			StringWriter stringRequest = new StringWriter();
 			try {
-				JAXBContext.newInstance(oasis.names.tc.xacml._3_0.core.schema.wd_17.Request.class).createMarshaller()
-						.marshal(xacmlRequest, stringRequest);
+				Marshaller marshaller = JAXBContext
+						.newInstance(oasis.names.tc.xacml._3_0.core.schema.wd_17.Request.class).createMarshaller();
+				marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+				marshaller.marshal(xacmlRequest, stringRequest);
 			} catch (JAXBException e) {
 				e.printStackTrace();
 				LOGGER.error(e.getLocalizedMessage());
@@ -101,36 +109,56 @@ public final class Utils {
 	}
 
 	private static void check(Category category) throws XacmlSdkException {
-
+		if (null == category) {
+			throw new XacmlSdkException(XacmlSdkExceptionCodes.CATEGORY_IS_NULL);
+		}
 	}
 
-	public static Responses extractResponse(Response myResponse) {
+	/**
+	 * This method take a XACML Response and convert it to a Responses object
+	 * easy to use in the SDK
+	 * 
+	 * @param myResponse
+	 * @return
+	 * @throws XacmlSdkException 
+	 */
+	public static ResponsesFactory extractResponse(Response myResponse) throws XacmlSdkException {
 
-		Responses responses = new Responses();
-		// FIXME: possible NPE on each of the getContent
+		com.thalesgroup.authzforce.sdk.core.schema.Responses responses = new com.thalesgroup.authzforce.sdk.core.schema.Responses();
+		Attribute subject = new Attribute();
+		Attribute resource = new Attribute();
+		Attribute action = new Attribute();
+
 		for (Result result : myResponse.getResults()) {
 			com.thalesgroup.authzforce.sdk.core.schema.Response response = new com.thalesgroup.authzforce.sdk.core.schema.Response();
-			for (Attributes returnedAttr : result.getAttributes()) {
-				for (Attribute attr : returnedAttr.getAttributes()) {
+			if(result.getDecision().equals(DecisionType.INDETERMINATE) || result.getDecision().equals(DecisionType.NOT_APPLICABLE)) {
+				throw new XacmlSdkException("Decision is " + result.getDecision().value());
+			}
+			response.setDecision(result.getDecision());
+			for (Attributes attrs : result.getAttributes()) {
+				for (Attribute attr : attrs.getAttributes()) {
 					if (attr.getAttributeId().equals(XACMLAttributeId.XACML_RESOURCE_RESOURCE_ID.value())) {
-						for (AttributeValueType attrValue : attr.getAttributeValues()) {
-							response.setResourceId(String.valueOf(attrValue.getContent().get(0)));
+						if (null != attr.getAttributeValues() && attr.getAttributeValues().size() > 0) {
+							response.setSubject(String.valueOf(attr.getAttributeValues().get(0).getContent()));
 						}
 					} else if (attr.getAttributeId().equals(XACMLAttributeId.XACML_ACTION_ACTION_ID.value())) {
-						for (AttributeValueType attrValue : attr.getAttributeValues()) {
-							response.setAction(String.valueOf(attrValue.getContent().get(0)));
+						if (null != attr.getAttributeValues() && attr.getAttributeValues().size() > 0) {
+							response.setAction(String.valueOf(attr.getAttributeValues().get(0).getContent()));
+
 						}
 					} else if (attr.getAttributeId().equals(XACMLAttributeId.XACML_SUBJECT_SUBJECT_ID.value())) {
-						for (AttributeValueType attrValue : attr.getAttributeValues()) {
-							response.setSubject(String.valueOf(attrValue.getContent().get(0)));
+						if (null != attr.getAttributeValues() && attr.getAttributeValues().size() > 0) {
+							response.setResourceId(String.valueOf(attr.getAttributeValues().get(0).getContent()));
 						}
 					}
 				}
 			}
-			response.setDecision(result.getDecision());
-			responses.getResponse().add(response);
+			// responses.getResponses().put(attr.getAttributeValues().get(0).getContent().toString(),
+			// response);
+			responses.getResponses().add(response);
 		}
-		return responses;
+
+		return new ResponsesFactory(responses);
 	}
 
 	public static void logRawResponse(Response myResponse) {
